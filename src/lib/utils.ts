2d8266cc9feac8d30a056bf1e026e545f68deb78
Christian Kozalla <christian.kozalla@gmail.com>
2023-02-22 19:47:34 +0100

    sort post entries by date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,6 @@
+import type { EntryWithLinkResolutionAndWithUnresolvableLinks } from "contentful";
+import type { Post } from "./contentful";
+
 export function debounce(handler: (event: Event) => void, delay = 500) {
   let timeout: NodeJS.Timeout;
 
@@ -22,3 +25,10 @@ export function throttle(handler: (event: Event) => void, { maxCalls = 50, durat
     }
   };
 }
+
+export function sortPostsByDate(a: EntryWithLinkResolutionAndWithUnresolvableLinks<Post>, b: EntryWithLinkResolutionAndWithUnresolvableLinks<Post>) {
+    if (!a.fields.published || !b.fields.published) return 0;
+    const aDate = new Date(a.fields.published);
+    const bDate = new Date(b.fields.published);
+    return bDate.getTime() - aDate.getTime();
+  }
