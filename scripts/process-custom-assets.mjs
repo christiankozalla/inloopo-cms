3b298dee0836759ec40fa0f4bfb32a06be1dc171
Christian Kozalla <christian.kozalla@gmail.com>
2023-04-08 23:33:29 +0200

    feat: integration for post-processing build assets (sitemap, dynamic-chart-core.src.js)

---
+++ scripts/process-custom-assets.mjs
@@ -0,0 +1,23 @@
+
+import { exec } from "node:child_process";
+import { join } from "node:path";
+
+export const processCustomAssets = {
+  name: "process-custom-assets",
+  hooks: {
+    "astro:build:done": (options) => {
+      exec('esbuild ' + join(options.dir.pathname, "scripts", "dynamic-chart-core.src.js") + ' --bundle --outfile=' + join(options.dir.pathname, "scripts", "dynamic-chart-core.js") + ' --platform=browser --target=es2015 --minify --define:process.env.NODE_ENV="production"', (err, stdout, stderr) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      });
+      exec(`${join(options.dir.pathname, "..", "scripts", "after-build.sh")}`, (err, stdout, stderr) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      });
+    },
+  },
+};
