d6497c4e2e9a6a504ae815028e0189b5dfd35425
Christian Kozalla <christian.kozalla@gmail.com>
2023-06-04 12:32:12 +0200

    chore(asset-pipeline): compile dynamic-chart-core.src.js in dev mode too

--- scripts/process-custom-assets.mjs
+++ scripts/process-custom-assets.mjs
@@ -1,29 +1,37 @@
 import { exec } from "node:child_process";
 import { join } from "node:path";
 
+const compileDynamicChartHook = (options) => {
+  const isBuidStep = options?.dir?.pathname;
+  const pathToSrcScript = isBuidStep? join(options.dir.pathname, "scripts", "dynamic-chart-core.src.js") : join(process.cwd(), "public", "scripts", "dynamic-chart-core.src.js");
+  const pathToOutScript = isBuidStep ? join(options.dir.pathname, "scripts", "dynamic-chart-core.js") : join(process.cwd(), "public", "scripts", "dynamic-chart-core.js");
+  exec(
+    "esbuild " +
+    pathToSrcScript +
+      " --bundle --outfile=" +
+      pathToOutScript +
+      ` --platform=browser --target=es2015 --minify ${isBuidStep ? "" : " --watch"}`,
+    (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    }
+  );
+  if (isBuidStep) {
+    exec(`${join(options.dir.pathname, "..", "scripts", "after-build.sh")}`, (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+  }
+};
+
 export const processCustomAssets = {
   name: "process-custom-assets",
   hooks: {
-    "astro:build:done": (options) => {
-      exec(
-        "esbuild " +
-          join(options.dir.pathname, "scripts", "dynamic-chart-core.src.js") +
-          " --bundle --outfile=" +
-          join(options.dir.pathname, "scripts", "dynamic-chart-core.js") +
-          " --platform=browser --target=es2015 --minify",
-        (err, stdout, stderr) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-        }
-      );
-      exec(`${join(options.dir.pathname, "..", "scripts", "after-build.sh")}`, (err, stdout, stderr) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-      });
-    },
+    "astro:server:start": compileDynamicChartHook,
+    "astro:build:done": compileDynamicChartHook
   },
 };
