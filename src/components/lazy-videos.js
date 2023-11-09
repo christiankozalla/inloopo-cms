241926212f0f46abc6d9bac0eecb0adbce642c57
Christian Kozalla <christian.kozalla@gmail.com>
2023-11-09 10:47:54 +0100

    lazy load course videos and hide download button in video controls

--- src/components/lazy-videos.js
+++ src/components/lazy-videos.js
@@ -1,15 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
   var lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
 
+  const breakPoints = {
+    small: 640, // 40em
+    medium: 960, // 60em
+  };
+
   if ("IntersectionObserver" in window) {
-    var lazyVideoObserver = new IntersectionObserver(function (entries, observer) {
+    var lazyVideoObserver = new IntersectionObserver(function (entries, _observer) {
       entries.forEach(function (video) {
         if (video.isIntersecting) {
+          const screenWidth = window.innerWidth;
+          const wantedVideoSize =
+            screenWidth < breakPoints.small ? "small" : screenWidth < breakPoints.medium ? "medium" : "large";
+          let enabledVideo = false;
           for (var source in video.target.children) {
             var videoSource = video.target.children[source];
-            if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+            if (
+              typeof videoSource.tagName === "string" &&
+              videoSource.tagName === "SOURCE" &&
+              videoSource.dataset.size === wantedVideoSize
+            ) {
               videoSource.src = videoSource.dataset.src;
+              enabledVideo = true;
+              break;
             }
+          }
+
+          if (!enabledVideo) {
+            video.target.src = video.target.dataset.src;
           }
 
           video.target.load();
