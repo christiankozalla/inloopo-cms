c0d29880e9b4998893eff39b84d3b3cb0e2b9460
Christian Kozalla <christian.kozalla@gmail.com>
2024-01-06 14:19:11 +0100

    update dynamic chart data

---
+++ scripts/dynamic-chart-data/extract-dax-dow-nasdaq.js
@@ -0,0 +1,34 @@
+const { write } = require("fs");
+const fs = require("node:fs");
+const path = require("node:path");
+const [pathToCsv] = process.argv.slice(2);
+
+fs.readFile(path.join(__dirname, pathToCsv), { encoding: "utf8" }, (err, data) => {
+    if (err) {
+        throw new Error("Error reading file", err);
+    }
+    const rows = data.split("\n");
+    const dax = rows.map((row) => row.split(";")).map((row) => toNumber(row[3]));
+    const dow = rows.map((row) => row.split(";")).map((row) => toNumber(row[4]));
+    const nasdaq = rows.map((row) => row.split(";")).map((row) => toNumber(row[5]));
+
+    writeFile("dax.json", dax);
+    writeFile("dow.json", dow);
+    writeFile("nasdaq.json", nasdaq);
+});
+
+
+function toNumber(str) {
+    return Number(str?.replace("%", ""));
+}
+
+
+function writeFile(fileName, data) {
+    fs.writeFile(path.join(__dirname, fileName), JSON.stringify(data), "utf8", (err) => {
+        if (err) {
+            throw new Error(err);
+        } else {
+            console.log("File written to", path.join(__dirname, fileName));
+        }
+    });
+}
