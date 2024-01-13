6c66d4bde1f7f215e257e30ca3c8b3ac12a04d02
Christian Kozalla <christian.kozalla@gmail.com>
2024-01-13 10:12:10 +0100

    blog: fullscreen images onclick

---
+++ src/components/fullscreen-images.ts
@@ -0,0 +1,41 @@
+function toggleImageFullscreenOnBody(bodyEl: HTMLElement) {
+    if (bodyEl.classList.contains("fullscreen-mode")) {
+      bodyEl.classList.remove("fullscreen-mode");
+    } else {
+      bodyEl.classList.add("fullscreen-mode");
+    }
+  }
+
+  function toggleFullscreenOnImg(el: HTMLElement) {
+    if (el.classList.contains("fullscreen")) {
+      el.classList.remove("fullscreen");
+    } else {
+      el.classList.add("fullscreen");
+    }
+  }
+
+  document.addEventListener("click", function (e) {
+    if (window.innerWidth < 760) {
+      return;
+    }
+    const el = e.target as HTMLElement;
+    if (el.tagName.toUpperCase() === "IMG") {
+      toggleFullscreenOnImg(el);
+      toggleImageFullscreenOnBody(this.body);
+    }
+  });
+
+  document.addEventListener("keyup", function (e) {
+    if (window.innerWidth < 760) {
+      return;
+    }
+    if (e.key.toUpperCase() === "ESCAPE") {
+      const els = document.querySelectorAll(".fullscreen");
+      if (els.length > 0) {
+        toggleImageFullscreenOnBody(this.body);
+        for (const el of els) {
+          el.classList.remove("fullscreen");
+        }
+      }
+    }
+  });
