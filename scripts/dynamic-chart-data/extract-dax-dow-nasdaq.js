d90bb879139d43c5d2b897f5592acbfd1cee5721
Christian Kozalla <christian.kozalla@gmail.com>
2024-05-08 20:28:37 +0200

    format code

--- scripts/dynamic-chart-data/extract-dax-dow-nasdaq.js
+++ scripts/dynamic-chart-data/extract-dax-dow-nasdaq.js
@@ -4,31 +4,29 @@ const path = require("node:path");
 const [pathToCsv] = process.argv.slice(2);
 
 fs.readFile(path.join(__dirname, pathToCsv), { encoding: "utf8" }, (err, data) => {
-    if (err) {
-        throw new Error("Error reading file", err);
-    }
-    const rows = data.split("\n");
-    const dax = rows.map((row) => row.split(";")).map((row) => toNumber(row[3]));
-    const dow = rows.map((row) => row.split(";")).map((row) => toNumber(row[4]));
-    const nasdaq = rows.map((row) => row.split(";")).map((row) => toNumber(row[5]));
+  if (err) {
+    throw new Error("Error reading file", err);
+  }
+  const rows = data.split("\n");
+  const dax = rows.map((row) => row.split(";")).map((row) => toNumber(row[3]));
+  const dow = rows.map((row) => row.split(";")).map((row) => toNumber(row[4]));
+  const nasdaq = rows.map((row) => row.split(";")).map((row) => toNumber(row[5]));
 
-    writeFile("dax.json", dax);
-    writeFile("dow.json", dow);
-    writeFile("nasdaq.json", nasdaq);
+  writeFile("dax.json", dax);
+  writeFile("dow.json", dow);
+  writeFile("nasdaq.json", nasdaq);
 });
 
-
 function toNumber(str) {
-    return Number(str?.replace("%", ""));
+  return Number(str?.replace("%", ""));
 }
 
-
 function writeFile(fileName, data) {
-    fs.writeFile(path.join(__dirname, fileName), JSON.stringify(data), "utf8", (err) => {
-        if (err) {
-            throw new Error(err);
-        } else {
-            console.log("File written to", path.join(__dirname, fileName));
-        }
-    });
+  fs.writeFile(path.join(__dirname, fileName), JSON.stringify(data), "utf8", (err) => {
+    if (err) {
+      throw new Error(err);
+    } else {
+      console.log("File written to", path.join(__dirname, fileName));
+    }
+  });
 }
