339cbed978e641e3f5b40b4b9ede7657490fb09c
Christian Kozalla <christian.kozalla@gmail.com>
2024-05-09 22:28:27 +0200

    remove one linebreak in video-sitemap generation

--- scripts/video-sitemap.mjs
+++ scripts/video-sitemap.mjs
@@ -64,8 +64,7 @@ export function generateVideoSitemap({ sitemapFilename, filter = () => true }) {
 
                 const sitemapContent = `
                     <?xml version="1.0" encoding="UTF-8"?>
-                    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-                        xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+                    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
                             ${xmlForEachPage.join("\n")}
                     </urlset>`;
 
